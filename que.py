a0cb5b4e292848704e5edff97bc80b7b46271d70
javaughn-bradford <javaughn.bradford001@albright.edu>
2025-10-29 12:23:39 -0400

    Add FIFO queue methods to Queue class
    Implemented push, pop, and peek methods for the Queue class.

--- que.py
+++ que.py
@@ -1,10 +1,27 @@
 # que.py
 # A First In First Out (FIFO) queue
-# Modified by:
+# Modified by:javaughn bradford 
 # Note: Please write this yourself, not using an LLM.
 from sequential_collection import SequentialCollection
 
 
 class Queue(SequentialCollection):
-    # YOUR CODE HERE
+
+    def push(self, item):
+        # add to the end of the queue
+        self._data.append(item)
+
+    def pop(self):
+        # remove and return from the front
+        if len(self._data) == 0:
+            raise IndexError("pop from empty queue")
+        return self._data.pop(0)
+
+    def peek(self):
+        # look at the first item without removing it
+        if len(self._data) == 0:
+            raise IndexError("peek from empty queue")
+        return self._data[0]
+
+
     pass
