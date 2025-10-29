d93648d936822b456cad70df45988cef99df77c6
javaughn-bradford <javaughn.bradford001@albright.edu>
2025-10-29 12:24:29 -0400

    Add stack methods: push, pop, and peek
    Implemented push, pop, and peek methods for stack functionality.

--- stack.py
+++ stack.py
@@ -1,9 +1,25 @@
 # stack.py
 # A Last In First Out (LIFO) stack
-# Modified by:
+# Modified by:jaaughn bradford 
 # Note: Please write this yourself, not using an LLM.
 from sequential_collection import SequentialCollection
 
 class Stack(SequentialCollection):
-    # YOUR CODE HERE
+
+    def push(self, item):
+        # add to the top of the stack
+        self._data.append(item)
+
+    def pop(self):
+        # remove and return the top item
+        if len(self._data) == 0:
+            raise IndexError("pop from empty stack")
+        return self._data.pop()
+
+    def peek(self):
+        # look at the top item without removing it
+        if len(self._data) == 0:
+            raise IndexError("peek from empty stack")
+        return self._data[-1]
+
     pass
